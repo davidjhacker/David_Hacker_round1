803546441c0303edefb6ab5729056ee2f4c284e2
David Hacker <davidhacker@dhcp-10-250-182-208.harvard.edu>
2020-02-18 23:50:16 -0500

    b

--- scrape.py
+++ scrape.py
@@ -1,16 +1,15 @@
 import requests
+import json
+import emoji
 from bs4 import BeautifulSoup
+def bify(word):
+    b = list(word)
+    b[0] = "🅱️"
+    b = "".join(b)
+    return b
 page = requests.get("https://www.thecrimson.com/")
 soup = BeautifulSoup(page.content, 'html.parser')
 headlines = []
-def sarcastic(string):
-    output = string[0]
-    for i in range (1, len(string)):
-        if output[i-1].isupper():
-            output += (string[i].lower())
-        else:
-            output += (string[i].upper())
-    return output
 for headline in soup.find_all("h1"):
     headlines.append(headline.get_text())
 headlines.remove("Sections")
@@ -18,5 +17,17 @@ headlines.remove("About")
 headlines.remove("Resources")
 f = open("davidoutput.txt", "w")
 for headline in headlines:
-    f.write(sarcastic(headline) + "\n")
+    f.write(headline + " --> ")
+    for word in headline.split():
+        page = requests.get("https://api.datamuse.com/words?ml=" + word + "&sp=b*&max=10")
+        dictlist = json.loads(page.content)
+        if word == "Harvard":
+            f.write("🅱️arvard ")
+        elif word == "of" or word == "on":
+            f.write(word + " ")
+        elif len(dictlist) == 0:
+            f.write(word + " ")
+        else:
+            f.write(bify(dictlist[0]["word"]) + " ")
+    f.write("\n")
 f.close()
